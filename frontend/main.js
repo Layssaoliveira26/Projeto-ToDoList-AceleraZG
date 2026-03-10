7b98660ba553081d11d6685a35e951e0ea13234c
Layssa Oliveira <layssaoliveira626@gmail.com>
2026-03-09 21:15:05 -0300

    feat:construida função de listagem de tarefas

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,5 @@
-const registroTarefas = [];
+let registroTarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let listagemTarefas = document.getElementsByClassName('listaTarefas');
 
 function salvarTarefa(event) {
     event.preventDefault();
@@ -10,17 +11,32 @@ function salvarTarefa(event) {
         status: document.getElementById('statusTarefa').value
     };
     registroTarefas.push(dados);
+    localStorage.setItem('tarefas', JSON.stringify(registroTarefas));
     console.log(registroTarefas);
 
+    limparFormulario();
+}
+
+function listarTarefas() {
+    listagemTarefas[0].innerHTML = '';
+    for(let i = 0; i < registroTarefas.length; i++) {
+        let tarefa = registroTarefas[i];
+            listagemTarefas[0].innerHTML += `
+                <div class="tarefa">
+                    <h4>${tarefa.titulo}</h4>
+                    <p>Descrição: ${tarefa.descricao}</p>
+                    <p>Data: ${tarefa.dataTermino}</p>
+                    <p>Categoria: ${tarefa.categoria}</p>
+                    <p>Status: ${tarefa.status}</p>
+                </div>
+            `;
+        }
+}
+
+function limparFormulario() {
     document.getElementById('tituloTarefa').value = '';
     document.getElementById('descricaoTarefa').value = '';
     document.getElementById('dataTerminoTarefa').value = '';
     document.getElementById('categoriaTarefa').value = '';
     document.getElementById('statusTarefa').value = '';
-}
-
-function listarTarefas() {
-    for( tarefa in registroTarefas ) {
-        console.log(tarefa)
-    }
 }
